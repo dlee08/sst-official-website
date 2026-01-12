a5c6ec9f006f509c3d39095f3ac11a3ddfb48b4a
dlee08 <dlee08.nyc@gmail.com>
2026-01-12 18:10:46 -0500

    Fine-tune timeline scroll offset to 70%

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -26,7 +26,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   const { scrollYProgress } = useScroll({
     target: containerRef,
-    offset: ["start 10%", "end 80%"],
+    offset: ["start 10%", "end 70%"],
   });
 
   const heightTransform = useTransform(scrollYProgress, [0, 1], [0, height]);
